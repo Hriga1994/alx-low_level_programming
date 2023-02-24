a2a6cdffa05132a4572ebc4456c684e85f9681e1
Hriga1994 <hassen-20@live.fr>
2023-02-23 18:10:34 -0800

    debuging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,54 +1,50 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* Return: day of year
-*/
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 
-int convert_day(int month, int day)
+void print_remaining_days(int month, int day, int year);
+
+/**
+ * print_remaining_days - prints the remaining days
+ * @month: the current month
+ * @day: the current day
+ * @year: the current year
+ * Return: void
+ */
+
+void print_remaining_days(int month, int day, int year)
 {
-	switch (month)
+	if ((year % 4 == 0) || (year % 100 == 0 && year % 400 == 0))
 	{
-	case 2:
-		day = 31 + day;
-		break;
-	case 3:
-		day = 59 + day;
-		break;
-	case 4:
-		day = 90 + day;
-		break;
-	case 5:
-		day = 120 + day;
-		break;
-	case 6:
-		day = 151 + day;
-		break;
-	case 7:
-		day = 181 + day;
-		break;
-	case 8:
-		day = 212 + day;
-		break;
-	case 9:
-		day = 243 + day;
-		break;
-	case 10:
-		day = 273 + day;
-		break;
-	case 11:
-		day = 304 + day;
-		break;
-	case 12:
-		day = 334 + day;
-		break;
-	default:
-		break;
+		int leap_days = 366;
+
+		if (day >= 60 && month > 2)
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", leap_days - day);
 	}
-	return (day);
+	else
+	{
+		if (day == 60 && month == 2)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			int standard_days = 365;
+
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", standard_days - day);
+		}
+	}
 }
-
-
